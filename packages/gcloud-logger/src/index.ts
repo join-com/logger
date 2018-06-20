8b41de7b564aee6fc32fe55a0fb9c8734c47fa55
Anton Versal <ant.ver@gmail.com>
2018-06-20 13:01:02 +0200

    fixes naming and adding index

---
+++ packages/gcloud-logger/src/index.ts
@@ -0,0 +1,6 @@
+import { Logger } from './Logger';
+
+const USE_JSON_FORMAT = process.env.NODE_ENV === 'production';
+const logger = new Logger(USE_JSON_FORMAT, process.env.LOG_LEVEL);
+
+export default logger;
