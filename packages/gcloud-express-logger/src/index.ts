165190e2718bad397d97ff113e7cf7b180cc74f6
Anton Versal <ant.ver@gmail.com>
2018-06-20 16:10:11 +0200

    adds express logger

---
+++ packages/gcloud-express-logger/src/index.ts
@@ -0,0 +1,47 @@
+import gcloudLogger, { Logger } from '@join-com/gcloud-logger';
+import { NextFunction, Request, Response } from 'express';
+import onFinished from 'on-finished';
+
+const requestLogMessage = (req: Request, res: Response, ms: number) => ({
+  httpRequest: {
+    remoteIp: req.ip,
+    requestUrl: req.originalUrl || req.url,
+    requestMethod: req.method,
+    status: res.statusCode,
+    userAgent: req.headers['user-agent'],
+    referer: req.headers.referer || req.headers.referrer,
+    latency: `${ms.toFixed(3)}ms`,
+  },
+  query: req.query,
+  reqBody: req.body,
+  requestTime: ms,
+});
+
+export const requestLogger = (
+  logger: Logger = gcloudLogger,
+  startTrace?: (req: Request) => void,
+) => (req: Request, res: Response, next: NextFunction) => {
+  const startTime = process.hrtime();
+
+  const logRequest = () => {
+    if (startTrace) {
+      startTrace(req);
+    }
+    const diff = process.hrtime(startTime);
+    const ms = diff[0] * 1e3 + diff[1] * 1e-6;
+    const message = requestLogMessage(req, res, ms);
+    if (res.statusCode >= 500) {
+      logger.error(req.path, message);
+    } else if (res.statusCode >= 400) {
+      logger.warn(req.path, message);
+    } else {
+      logger.info(req.path, message);
+    }
+  };
+  if (req.originalUrl !== '/healthz' && req.originalUrl !== '/readiness') {
+    onFinished(res, logRequest);
+  }
+  next();
+};
+
+export default requestLogger;
