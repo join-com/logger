3b736e8260f567c0ec11f3b741723fa01c43ff6e
kubaSpringer <jakub.springer@join.com>
2021-09-01 13:05:54 +0200

    [JOIN-13606] Add buildId information to logger from http request header (#54)
    * [JOIN-13606] Add buildId information to logger from http request header
    
    [JOIN-13606]
    
    * lowercase
    
    * Replace build id with release

--- packages/gcloud-express-logger/src/index.ts
+++ packages/gcloud-express-logger/src/index.ts
@@ -9,6 +9,7 @@ const requestLogMessage = (req: Request, res: Response, ms: number) => ({
     requestMethod: req.method,
     status: res.statusCode,
     userAgent: req.headers['user-agent'],
+    release: req.headers['release'],
     referer: req.headers.referer || req.headers.referrer,
     latency: `${ms.toFixed(3)}ms`,
     path: req.route && req.route.path,
