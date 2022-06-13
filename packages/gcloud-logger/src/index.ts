81cc1362b47bacacef8439f8059073ece1af8d43
Evgeniy Kirpichenko <evkirpichenko@gmail.com>
2022-06-13 15:07:22 +0200

    fix: Export logger

--- packages/gcloud-logger/src/index.ts
+++ packages/gcloud-logger/src/index.ts
@@ -1,7 +1,5 @@
 import { Logger } from './Logger'
 
-const USE_JSON_FORMAT = process.env['NODE_ENV'] === 'production'
-const logger = new Logger(USE_JSON_FORMAT, process.env['LOG_LEVEL'])
-
 export { Logger, Level } from './Logger'
-export default logger
+
+export const logger = new Logger(process.env['NODE_ENV'] === 'production', process.env['LOG_LEVEL'])
