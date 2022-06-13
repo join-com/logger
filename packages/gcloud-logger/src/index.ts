548dbc1045a0be859d36646195ea68269d0f12c7
Evgeniy Kirpichenko <evkirpichenko@gmail.com>
2022-06-13 15:22:18 +0200

    fix: Export reportError

--- packages/gcloud-logger/src/index.ts
+++ packages/gcloud-logger/src/index.ts
@@ -3,3 +3,7 @@ import { Logger } from './Logger'
 export { Logger, Level } from './Logger'
 
 export const logger = new Logger(process.env['NODE_ENV'] === 'production', process.env['LOG_LEVEL'])
+
+export const reportError = (e: unknown) => {
+  logger.reportError(e)
+}
