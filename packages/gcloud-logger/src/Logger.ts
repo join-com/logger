9af7498c5eea64fdf19fdb56462ab424002ed129
Eugene Taran <eugene.taran@join.com>
2023-03-27 17:02:30 +0200

    pino try

--- packages/gcloud-logger/src/Logger.ts
+++ packages/gcloud-logger/src/Logger.ts
@@ -1,39 +1,57 @@
 import Pino from 'pino'
 
 export enum Level {
-  DEFAULT = 'DEFAULT',
   DEBUG = 'DEBUG',
   INFO = 'INFO',
-  NOTICE = 'NOTICE',
   WARNING = 'WARNING',
   ERROR = 'ERROR',
   CRITICAL = 'CRITICAL',
-  ALERT = 'ALERT',
-  EMERGENCY = 'EMERGENCY',
 }
+
+// https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#logseverity
+const PinoLevelToCloudSeverityLookup = new Map<string,string>(Object.entries({
+  debug: 'DEBUG',
+  info: 'INFO',
+  warn: 'WARNING',
+  error: 'ERROR',
+  fatal: 'CRITICAL',
+}));
+
+// https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry
+const PinoLevelToCloudLevelLookup = new Map<string,number>(Object.entries({
+  debug: 100,
+  info: 200,
+  warn: 400,
+  error: 500,
+  fatal: 600,
+}));
 
 export class Logger {
   private readonly logger: Pino.Logger
 
   constructor(
+    usePrettyPrint: boolean,
     logLevelStarts?: string,
-    // private readonly excludeKeys = ['password', 'token', 'newPassword', 'oldPassword'],
+    excludeKeys = ['password', 'token', 'newPassword', 'oldPassword']
   ) {
+    const defaultPinoConf = {
+      messageKey: 'message',
+      formatters: {
+        messageKey: 'message',
+        level: (label: string, _number: number) => {
+          return {
+            severity: PinoLevelToCloudSeverityLookup.get(label) || PinoLevelToCloudSeverityLookup.get(label),
+            level: PinoLevelToCloudLevelLookup.get(label),
+          }
+        },
+      },
+    }
+    const transport = usePrettyPrint?  { target: 'pino-pretty' } : { target: '' }
     this.logger = Pino({
       level: logLevelStarts,
-      transport: {
-        target: 'pino-pretty'
-      },
-      customLevels: {
-        debug: 100,
-        info: 200,
-        notice: 300,
-        warning: 400,
-        error: 500,
-        critical: 600,
-        alert: 700,
-        emergency: 800
-      }
+      redact: excludeKeys,
+      defaultPinoConf,
+      transport,
     })
   }
 
@@ -45,10 +63,6 @@ export class Logger {
     this.logger.info(payload, message)
   }
 
-  public notice(message: string, payload?: unknown) {
-    this.logger.debug(payload, message)
-  }
-
   public warn(message: string, payload?: unknown) {
     this.logger.warn(payload, message)
   }
@@ -57,16 +71,7 @@ export class Logger {
     this.logger.error(payload, message)
   }
 
-  public crit(message: string, payload?: unknown) {
-    this.logger.fatal(payload, message)
-  }
-
-  public alert(message: string, payload?: unknown) {
-    this.logger.fatal(payload, message)
-  }
-
-  
-  public emerg(message: string, payload?: unknown) {
+  public critical(message: string, payload?: unknown) {
     this.logger.fatal(payload, message)
   }
 
@@ -79,123 +84,10 @@ export class Logger {
   }
 
 
-
-  // private stringify(message: unknown) {
-  //   // https://gist.github.com/saitonakamura/d51aa672c929e35cc81fa5a0e31f12a9
-  //   const replaceCircular = (obj: any, alreadySeen = new WeakSet()): any => {
-  //     if (typeof obj !== 'object') {
-  //       return obj
-  //     }
-  //
-  //     if (!obj) {
-  //       return obj
-  //     }
-  //
-  //     if (alreadySeen.has(obj)) {
-  //       return '[CIRCULAR]'
-  //     }
-  //
-  //     alreadySeen.add(obj)
-  //
-  //     if (typeof obj.pipe === 'function') {
-  //       return '[object Stream]'
-  //     }
-  //
-  //     if (Buffer.isBuffer(obj)) {
-  //       return '[object Buffer]'
-  //     }
-  //
-  //     if (Array.isArray(obj)) {
-  //       return obj.map(item => replaceCircular(item, alreadySeen))
-  //     }
-  //
-  //     const serializedObj = obj instanceof Error ? this.serializeError(obj) : obj
-  //     const keys = Object.keys(serializedObj)
-  //     if (keys.length === 0) {
-  //       return obj
-  //     }
-  //
-  //     const newObj: Record<string, unknown> = {}
-  //     keys.forEach(key => {
-  //       const val = replaceCircular(obj[key], alreadySeen)
-  //       newObj[key] = val
-  //     })
-  //
-  //     alreadySeen.delete(obj)
-  //     return newObj
-  //   }
-  //
-  //   const excludeSensitive = (key: string, value: any) => {
-  //     // exclude sensitive values
-  //     if (this.excludeKeys.indexOf(key) !== -1) {
-  //       return '[FILTERED]'
-  //     }
-  //     // simply return otherwise
-  //     return value
-  //   }
-  //
-  //   return JSON.stringify(replaceCircular(message), excludeSensitive)
-  // }
-
   private isError(err: unknown): err is Error {
     if (!err) {
       return false
     }
     return typeof err === 'object' && 'message' in err && 'stack' in err
   }
-
-  // private getPayloadObject(payload?: unknown): Record<string, unknown> | undefined {
-  //   if (!payload) {
-  //     return undefined
-  //   }
-  //
-  //   if (this.isError(payload)) {
-  //     return { error: payload }
-  //   }
-  //
-  //   if (this.isObject(payload)) {
-  //     return payload
-  //   }
-  //
-  //   return { payload }
-  // }
-  //
-  // private serializeError<E extends Error>(error: E): Record<string, unknown> {
-  //   const serializedError: Record<string, unknown> = {
-  //     name: error.name,
-  //     message: error.message,
-  //   }
-  //
-  //   if (error.stack) {
-  //     serializedError['stack'] = error.stack
-  //   }
-  //
-  //   // Possible Node.js (system error) properties...
-  //   if (this.hasProperty('code', error)) {
-  //     serializedError['code'] = error.code
-  //   }
-  //
-  //   if (this.hasProperty('errno', error)) {
-  //     serializedError['errno'] = error.errno
-  //   }
-  //
-  //   if (this.hasProperty('syscall', error)) {
-  //     serializedError['syscall'] = error.syscall
-  //   }
-  //
-  //   // Any enumerable properties...
-  //   for (const key in error) {
-  //     serializedError[key] = error[key]
-  //   }
-  //
-  //   return serializedError
-  // }
-
-  // private hasProperty<T extends string>(property: T, obj: unknown): obj is { [property in T]: unknown } {
-  //   return Boolean(obj && typeof obj === 'object' && property in obj)
-  // }
-  //
-  // private isObject(obj: unknown): obj is Record<string, unknown> {
-  //   return obj instanceof Object && obj.constructor === Object
-  // }
 }
--- packages/gcloud-logger/src/index.ts
+++ packages/gcloud-logger/src/index.ts
@@ -2,7 +2,7 @@ import { Logger } from './Logger'
 
 export { Logger, Level } from './Logger'
 
-export const logger = new Logger(process.env['LOG_LEVEL'])
+export const logger = new Logger(process.env['NODE_ENV'] === 'production', process.env['LOG_LEVEL'])
 
 export const reportError = (e: unknown) => {
   logger.reportError(e)
