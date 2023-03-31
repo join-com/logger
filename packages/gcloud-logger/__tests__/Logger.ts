28e9b4415450b81e8de2c56d4354abef77fe88ca
Eugene Taran <eugene.taran@join.com>
2023-03-31 09:19:23 +0200

    small test update, update yarn lock in root dir

--- packages/gcloud-logger/__tests__/Logger.ts
+++ packages/gcloud-logger/__tests__/Logger.ts
@@ -3,18 +3,18 @@ describe('logger', () => {
   it('truncates field over maxFieldLength', () => {
     const stdoutSpy = jest.spyOn(process.stdout, 'write');
 
-    const logger: Logger = new Logger(true, 'DEBUG', ['password', 'token', 'newPassword', 'oldPassword'], 5)
-    logger.debug('hm', { response: { some: 'longWord' } })
-    const log = '{"response":{"some":"longW...TRUNCATED"},"message":"hm","severity":"DEBUG","level":100}\n'
+    const logger = new Logger(true, 'DEBUG', [], 5)
+    logger.debug('msg', { response: { some: 'longWord' } })
+    const log = '{"response":{"some":"longW...TRUNCATED"},"message":"msg","severity":"DEBUG","level":100}\n'
     expect(stdoutSpy).toHaveBeenCalledWith(log);
   })
 
   it('hides field in excludeKeys', () => {
     const stdoutSpy = jest.spyOn(process.stdout, 'write');
 
-    const logger: Logger = new Logger(true, 'DEBUG', ['password', 'token', 'newPassword', 'oldPassword', 'some'], 5)
-    logger.debug('hm', { response: { some: 'longWord' } })
-    const log = '{"response":{"some":"[FILTERED]"},"message":"hm","severity":"DEBUG","level":100}\n'
+    const logger = new Logger(true, 'DEBUG', ['some'], 5)
+    logger.debug('msg', { response: { some: 'longWord' } })
+    const log = '{"response":{"some":"[FILTERED]"},"message":"msg","severity":"DEBUG","level":100}\n'
     expect(stdoutSpy).toHaveBeenCalledWith(log);
   })
 })
