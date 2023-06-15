b5876f841397d7285460e9f4ac14b5a350718ecb
Ievgen Kyrpychenko <evkirpichenko@gmail.com>
2023-06-15 16:53:51 +0200

    feat: JOIN-32560 allow configuring max field lenght via env variable

--- packages/gcloud-logger/src/index.ts
+++ packages/gcloud-logger/src/index.ts
@@ -2,7 +2,12 @@ import { Logger } from './Logger'
 
 export { Logger, Level } from './Logger'
 
-export const logger = new Logger(process.env['NODE_ENV'] === 'production', process.env['LOG_LEVEL'])
+export const logger = new Logger(
+  process.env['NODE_ENV'] === 'production',
+  process.env['LOG_LEVEL'],
+  undefined,
+  parseInt(process.env['LOG_MAX_FIELD_LENGTH'] ?? '', 10),
+)
 
 export const reportError = (e: unknown) => {
   logger.reportError(e)
