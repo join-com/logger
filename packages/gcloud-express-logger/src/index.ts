fe26284d397a2a18e143d50f5b9719cda84574f1
Ievgen Kyrpychenko <evkirpichenko@gmail.com>
2024-08-30 18:32:46 +0200

    feat: TECH make graphql requests more verbose (#105)
    * feat: TECH log graphql operation name
    
    * Log query unless operation name provided
    
    * fix: TECH trigger release

--- packages/gcloud-express-logger/src/index.ts
+++ packages/gcloud-express-logger/src/index.ts
@@ -13,12 +13,25 @@ const requestLogMessage = (req: Request, res: Response, ms: number) => ({
     transactionName: req.headers['transaction-name'],
     referer: req.headers.referer,
     latency: `${ms.toFixed(3)}ms`,
-    path: req.route && req.route.path,
   },
   query: req.query,
   reqBody: req.body,
   requestTime: ms,
 })
+
+const requestOperationName = (req: Request): string | undefined => {
+  if ('operationName' in req.body && typeof req.body.operationName === 'string') {
+    return req.body.operationName
+  }
+
+  if (req.originalUrl.endsWith('graphql') && 'query' in req.body && typeof req.body.query === 'string') {
+    const query = req.body.query as string
+    const match = query.match(/\s*(query|mutation)\s+(\w+)/)
+    return match ? match[2] : undefined
+  }
+
+  return undefined
+}
 
 export interface IGcloudLogger {
   info: (message: string, payload?: unknown) => void
@@ -26,28 +39,33 @@ export interface IGcloudLogger {
   error: (message: string, payload?: unknown) => void
 }
 
-export const requestLogger = (logger: IGcloudLogger) => (req: Request, res: Response, next: NextFunction): void => {
-  const startTime = process.hrtime()
+export const requestLogger =
+  (logger: IGcloudLogger) =>
+  (req: Request, res: Response, next: NextFunction): void => {
+    const startTime = process.hrtime()
 
-  const logRequest = () => {
-    const diff = process.hrtime(startTime)
-    const ms = diff[0] * 1e3 + diff[1] * 1e-6
-    const message = requestLogMessage(req, res, ms)
-    if (res.statusCode >= 500) {
-      logger.error(req.path, message)
-    } else if (res.statusCode >= 400) {
-      logger.warn(req.path, message)
-    } else {
-      logger.info(req.path, message)
+    const logRequest = () => {
+      const diff = process.hrtime(startTime)
+      const ms = diff[0] * 1e3 + diff[1] * 1e-6
+      const payload = requestLogMessage(req, res, ms)
+      const operationName = requestOperationName(req)
+      const message = operationName ? `${req.originalUrl} ${operationName}` : req.originalUrl
+
+      if (res.statusCode >= 500) {
+        logger.error(message, payload)
+      } else if (res.statusCode >= 400) {
+        logger.warn(message, payload)
+      } else {
+        logger.info(message, payload)
+      }
     }
+    if (
+      req.originalUrl !== '/healthz' &&
+      req.originalUrl !== '/readiness' &&
+      req.originalUrl !== '/api/healthz' &&
+      req.originalUrl !== '/api/readiness'
+    ) {
+      onFinished(res, logRequest)
+    }
+    next()
   }
-  if (
-    req.originalUrl !== '/healthz' &&
-    req.originalUrl !== '/readiness' &&
-    req.originalUrl !== '/api/healthz' &&
-    req.originalUrl !== '/api/readiness'
-  ) {
-    onFinished(res, logRequest)
-  }
-  next()
-}
